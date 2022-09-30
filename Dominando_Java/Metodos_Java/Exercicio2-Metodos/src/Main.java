4b8fa3bdbf2d90c6330fa19242c05e1441ad42d2
robertoCastro <bkastro@gmail.com>
2022-09-30 18:12:23 -0300

    Inclusão dos execícios da aula sobre métodos

---
+++ Dominando_Java/Metodos_Java/Exercicio2-Metodos/src/Main.java
@@ -0,0 +1,11 @@
+import com.digitalInnovation.bootcamp.Quadrilatero;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(" === Exercício sobrecargas da área === ");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 5);
+    }
+}
