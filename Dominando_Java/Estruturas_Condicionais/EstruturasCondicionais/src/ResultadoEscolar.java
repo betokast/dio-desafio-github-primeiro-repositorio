1bbd00aedf456f9b2a9ee9e4429acafd4f263543
robertoCastro <bkastro@gmail.com>
2022-10-01 15:13:52 -0300

    Refatoração da classe ResultadoEscolar com a condição ternária para Aprovado ou Reprovado

--- Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/ResultadoEscolar.java
+++ Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/ResultadoEscolar.java
@@ -1,13 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 4;
 
-        if(nota >= 7){
-            System.out.println("O aluno foi aprovado ");
-        } else if(nota >= 5 && nota < 7){
-            System.out.println("O aluno está na recuperação");
-        } else {
-            System.out.println("O aluno foi reprovado");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+
+//        if(nota >= 7){
+//            System.out.println("O aluno foi aprovado ");
+//        } else if(nota >= 5 && nota < 7){
+//            System.out.println("O aluno está na recuperação");
+//        } else {
+//            System.out.println("O aluno foi reprovado");
+//        }
     }
 }
