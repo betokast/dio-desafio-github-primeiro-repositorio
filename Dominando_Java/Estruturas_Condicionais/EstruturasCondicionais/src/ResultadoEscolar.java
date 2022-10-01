97e9d288f80aa302e51f134d9d49b0ac12599fdb
robertoCastro <bkastro@gmail.com>
2022-10-01 15:15:57 -0300

    Refatoração da classe ResultadoEscolar com a condição ternária para os cenários Aprovado, Recuperação ou Reprovado

--- Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/ResultadoEscolar.java
+++ Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/ResultadoEscolar.java
@@ -1,8 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 4;
+        int nota = 10;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
 
 
