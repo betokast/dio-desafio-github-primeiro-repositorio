e9af63bcb31de109bace3b44ac36ed3508b7e6ff
robertoCastro <bkastro@gmail.com>
2022-10-01 15:55:10 -0300

    Criação do pacote de laços de repetição e inclusão da classe ExemploFor

---
+++ Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/lacos/ExemploFor.java
@@ -0,0 +1,12 @@
+package lacos;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println("Carneirinho nº: " + carneirinhos);
+        }
+
+        System.out.println("Joãozinho dormiu");
+    }
+}
