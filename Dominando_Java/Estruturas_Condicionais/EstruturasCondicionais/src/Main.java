524779a9c3e5b44db36f4743c33da4ee95fd0cb2
robertoCastro <bkastro@gmail.com>
2022-10-01 17:49:41 -0300

    Inclusão da classe ExemploDiWhile

--- Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/Main.java
+++ Dominando_Java/Estruturas_Condicionais/EstruturasCondicionais/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        for(int x=1;x<=10;x++){
+            if(x % 2==0) //% operador que representa módulo de uma divisão
+                System.out.println(x);
+        }
     }
 }
